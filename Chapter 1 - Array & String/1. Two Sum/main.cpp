ff988211c29a85f52b16e634abf13d19b184fb77
lynnxlmiao <mxl925202@outlook.com>
2018-01-21 23:53:38 +0800

    commit

--- Chapter 1 - Array & String/1. Two Sum/main.cpp
+++ Chapter 1 - Array & String/1. Two Sum/main.cpp
@@ -9,7 +9,9 @@ You may assume that each input would have exactly one solution.
 Link: https://oj.leetcode.com/problems/two-sum/
  */
 
-class Solution {
+
+
+/*class Solution {
 public:
     vector<int> twoSum(vector<int> &numbers, int target) {
         vector<int> res;
@@ -41,4 +43,51 @@ public:
             }
         }
     }
+};*/
+
+/*#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+
+
+const int N = 200002;
+const int OFFSET = 100000;
+
+class Solution {
+private:
+    int idOfNum[N];
+public:
+    vector<int> twoSum(vector<int> &numbers, int target) {
+        vector<int> index;
+        memset(idOfNum, 0, sizeof(idOfNum));
+
+        int sz = numbers.size();
+        for (int i = 0; i < sz; i++) {
+            int rest = target - numbers[i];
+            if (idOfNum[rest + OFFSET]) {
+                index.push_back(idOfNum[rest + OFFSET]);
+                index.push_back(i + 1);
+                return index;
+            }
+            idOfNum[numbers[i] + OFFSET] = i + 1;
+        }
+        // Program never go here, because
+        // "each input would have exactly one solution"
+    }
 };
+
+int main() {
+    Solution s;
+    vector<int> in;
+    int n, t;
+    cin >> n;
+    while (n--) {
+        cin >> t;
+        in.push_back(t);
+    }
+    cin >> t;
+    in = s.twoSum(in, t);
+    cout << in[0] << ' ' << in[1] << endl;
+    return 0;
+}*/
